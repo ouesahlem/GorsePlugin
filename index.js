ffdb34c9543f6a948d0a2b9d8dd584815d1267a0
ouesahlem <102174962+ouesahlem@users.noreply.github.com>
2022-05-04 11:55:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,97 @@
-// <TODO: your plugin code here - you can base it on the code below, but you don't have to>
+import { createBuffer } from '@posthog/plugin-contrib'
+import { Plugin, PluginMeta, PluginEvent } from '@posthog/plugin-scaffold'
+import { Client } from 'pg'
 
-// Some internal library function
-async function getRandomNumber() {
-    return 4
+// fetch only declared, as it's provided as a plugin VM global
+declare function fetch(url: RequestInfo, init?: RequestInit): Promise<Response>
+
+export const metrics = {
+    'total_requests': 'sum',
+    'errors': 'sum'
 }
 
-// Plugin method that runs on plugin load
-export async function setupPlugin({ config }) {
-    console.log(`Setting up the plugin`)
+interface SendEventsPluginMeta extends PluginMeta {
+    cache: CacheExtension,
+    config: {
+        eventsToInclude: string
+        APIurl: string
+    },
+    global: {
+        pgClient: Client
+        eventsToInclude: Set<string>
+        buffer: ReturnType<typeof createBuffer>
+    }
+}
+    
+function verifyConfig({ config }: SendEventsPluginMeta) {
+    if (!config.APIurl) {
+        throw new Error('URL not provided!')
+    }
+
+    if (!/http:\/\/51.89.15.39:8087/(.+).test(config.APIurl)) {
+        throw new Error('Invalid URL')
+    }
+    
+    if (!config.eventsToInclude) {
+        throw new Error('No events to include!')
+    }
 }
 
-// Plugin method that processes event
-export async function processEvent(event, { config, cache }) {
-    const counterValue = (await cache.get('greeting_counter', 0))
-    cache.set('greeting_counter', counterValue + 1)
-    if (!event.properties) event.properties = {}
-    event.properties['greeting'] = config.greeting
-    event.properties['greeting_counter'] = counterValue
-    event.properties['random_number'] = await getRandomNumber()
-    return event
+async function sendEventToGorse(event: PluginEvent, meta: SendEventsPluginMeta) {
+
+    const { config, metrics } = meta
+
+    const types = (config.eventsToInclude || '').split(',')
+
+    if (!types.includes(event.event) || !event.properties) {
+        return
+    }
+
+    const token = await getToken(meta)
+
+    metrics.total_requests.increment(1)
+    const response = await fetch(
+        `http://51.89.15.39:8087/${config.APIurl}`,
+        {
+            method: config.eventMethodType,
+            headers: { 'accept: application/json', 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+            body: { 'Comment': '',
+                    'FeedbackType' : JSON.stringify(event.event),
+                    'ItemId' : JSON.stringify(event.properties.service_id),
+                    'Timestamp' : JSON.stringify(event.timestamp),
+                    'UserId' :  JSON.stringify(event.properties.anonymousId),
+                    
+        }
+    )
+    if (!statusOk(response)) {
+        metrics.errors.increment(1)
+        throw new Error(`Not a 200 response from event hook ${response.status}. Response: ${response}`)
+    }
+}
+
+export async function setupPlugin(meta: SendEventsPluginMeta) {
+    verifyConfig(meta)
+    const { global } = meta
+    global.buffer = createBuffer({
+        limit: 1024 * 1024, // 1 MB
+        timeoutSeconds: 1,
+        onFlush: async (events) => {
+            for (const event of events) {
+                await sendEventToGorse(event, meta)
+            }
+        },
+    })
+}
+
+export async function onEvent(event: PluginEvent, { global }: SendEventsPluginMeta) {
+    const eventSize = JSON.stringify(event).length
+    global.buffer.add(event, eventSize)
+}
+
+export function teardownPlugin({ global }: SendEventsPluginMeta) {
+    global.buffer.flush()
+}
+
+function statusOk(res: Response) {
+    return String(res.status)[0] === '2'
 }
