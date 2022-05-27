3d6197788e9289ea1175bab86f468817c019b7dd
ouesahlem <102174962+ouesahlem@users.noreply.github.com>
2022-05-27 10:38:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ export async function onEvent(event: PluginEvent, { global }: SendEventsPluginMe
 	var data=   JSON.stringify({ 
                                 'Comment': '',
                                 'FeedbackType' : event.event,
-                                'ItemId' : event.event,
+                                'ItemId' : event.properties?.item_id,
                                 'Timestamp' : event.properties?.timestamp,
                                 'UserId' :  event.distinct_id
                             });
@@ -129,7 +129,7 @@ export async function onEvent(event: PluginEvent, { global }: SendEventsPluginMe
                             'accept': 'application/json',
                             'Content-Type': 'application/json'
                         },
-                    body:'[{\"Comment\": \"ss\",  \"FeedbackType\": \"sss\",  \"ItemId\": \"ssss\",  \"Timestamp\": \"2022-05-26T17:21:32.838Z\",  \"UserId\": \"string\"}]'
+                    body:'[{\"Comment\": \"\",  \"FeedbackType\": \"${event.event}\",  \"ItemId\": \"${event.properties?.item_id}\",  \"Timestamp\": \"${event.properties?.timestamp}\",  \"UserId\": \"${event.distinct_id}\"}]'
                         
                     }
                 ).then((response) => response.json())
